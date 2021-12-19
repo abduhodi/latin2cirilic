45ec9ef6b1f6e8126e270cd8032b932ffda3b2a4
abduhodi <62491721+abduhodi@users.noreply.github.com>
2021-12-19 22:14:11 +0500

    Add files via upload

---
+++ latin2cirilic/main.py
@@ -0,0 +1,42 @@
+from transliterate import to_cyrillic, to_latin
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '1590392737:AAGBmrjevaMjnsrAty4S4TzMKEkZrfTmDhE'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Menga matn yuboring")
+
+@dp.message_handler(commands=['help'])
+async def send_help(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Lotin yozuvida matn yuborsangiz kiril yozuviga o`tkazib beraman\
+                        \nKiril yozuvida matn yuborsangiz lotin yozuviga o`tkazib beraman")
+
+
+@dp.message_handler()
+async def changer(message: types.Message):
+    msg = message.text
+    if msg.isascii():
+        msg = to_cyrillic(msg)
+    else:
+        msg = to_latin(msg)
+    await message.reply(msg)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
